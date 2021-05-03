6c668a8c22c4b56376512cc0fd478e2b89a7bedd
AlexFriis <alex3567@stud.kea.dk>
2021-05-03 16:59:36 +0200

    Updated TaskService & TeamService.

--- src/main/java/com/alexnmat/exam/service/TaskService.java
+++ src/main/java/com/alexnmat/exam/service/TaskService.java
@@ -1,7 +1,77 @@
 package com.alexnmat.exam.service;
 
+import com.alexnmat.exam.models.SubProject;
+import com.alexnmat.exam.models.Task;
+import com.alexnmat.exam.models.Team;
+import com.alexnmat.exam.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TaskService {
+
+    private SubProjectRepository subProjectRepository;
+
+    private ProjectRepository projectRepository;
+
+   private TaskRepository taskRepository;
+
+   private PersonRepository personRepository;
+
+   private HoursRepository hoursRepository;
+
+    public TaskService(SubProjectRepository subProjectRepository, ProjectRepository projectRepository, TaskRepository taskRepository, PersonRepository personRepository, HoursRepository hoursRepository) {
+        this.subProjectRepository = subProjectRepository;
+        this.projectRepository = projectRepository;
+        this.taskRepository = taskRepository;
+        this.personRepository = personRepository;
+        this.hoursRepository = hoursRepository;
+    }
+
+    public List<Task> findAllTasksForSubProjectId(long subProjectId) {
+        List<Task> fullList = taskRepository.findAll();
+        List<Task> taskList = new ArrayList<>();
+        for (int i = 0; i < fullList.size(); i++) {
+            if  {
+
+            }
+        }
+        return taskList;
+
+    }
+
+    public Task saveTask(Task task, long id) {
+        SubProject subProject = subProjectRepository.findById(id);
+        task.setSubProject(subProject);
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Task task, long id) {
+        taskRepository.findById(id);
+        boolean taskExists = true;
+        while(taskExists) {
+            if (taskRepository.findAll().contains(id)) {
+                task.getId().remove(task);
+            }
+            else {
+                taskExists = false;
+            }
+        }
+        taskRepository.delete(task);
+    }
+
+    public void completeTask(Task task, long id) {
+        taskRepository.findById(id);
+        task.setCompleted(true);
+    }
+
+    public int calculateTimeSpent() {
+
+    }
+
+
+
 }
--- src/main/java/com/alexnmat/exam/service/TeamService.java
+++ src/main/java/com/alexnmat/exam/service/TeamService.java
@@ -1,7 +1,48 @@
 package com.alexnmat.exam.service;
 
+import com.alexnmat.exam.models.Project;
+import com.alexnmat.exam.models.Team;
+import com.alexnmat.exam.repositories.ProjectRepository;
+import com.alexnmat.exam.repositories.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TeamService {
+
+    private TeamRepository teamRepository;
+
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    public TeamService(TeamRepository teamRepository, ProjectRepository projectRepository) {
+        this.teamRepository = teamRepository;
+        this.projectRepository = projectRepository;
+    }
+
+    public Team findByTeamId(long id) {
+        return teamRepository.findById(id);
+    }
+
+    public Team saveTeam(Team team, long id) {
+        Project project = projectRepository.findById(id);
+        team.setProject(project);
+        return teamRepository.save(team);
+    }
+
+    public void deleteTeam(long id) {
+        Team team = teamRepository.findById(id);
+        boolean teamExists = true;
+        while(teamExists) {
+            if (teamRepository.findAll().contains(id)) {
+                Team team = teamRepository.findById(id);
+                team.getId().remove(team);
+            }
+            else {
+                teamExists = false;
+            }
+        }
+        teamRepository.delete(team);
+    }
+
 }
