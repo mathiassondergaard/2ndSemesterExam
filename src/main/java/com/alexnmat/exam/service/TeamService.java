f0a5d1a5ff2c25640e6e280aefd2db4bca3b2e3c
AlexFriis <alex3567@stud.kea.dk>
2021-05-05 15:09:46 +0200

    Updated TeamService

--- src/main/java/com/alexnmat/exam/service/TeamService.java
+++ src/main/java/com/alexnmat/exam/service/TeamService.java
@@ -7,6 +7,8 @@ import com.alexnmat.exam.repositories.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.NoResultException;
+
 @Service
 public class TeamService {
 
@@ -20,22 +22,26 @@ public class TeamService {
         this.projectRepository = projectRepository;
     }
 
-    public Team findByTeamId(long id) {
-        return teamRepository.findById(id);
+    public Team findByTeamId(long teamId) {
+        return teamRepository.findById(teamId)
+                .orElseThrow(() -> new NoResultException("Unable to find team by id: " + teamId));
+
     }
 
-    public Team saveTeam(Team team, long id) {
-        Project project = projectRepository.findById(id);
+    public Team saveTeam(Team team, long projectId) {
+            Project project = projectRepository.findById(projectId)
+                    .orElseThrow(() -> new NoResultException("Unable to find project by id: " + projectId));
         team.setProject(project);
         return teamRepository.save(team);
     }
 
-    public void deleteTeam(long id) {
-        Team team = teamRepository.findById(id);
+    public void deleteTeam(long teamId) {
+        findByTeamId(teamId);
         boolean teamExists = true;
         while(teamExists) {
-            if (teamRepository.findAll().contains(id)) {
-                Team team = teamRepository.findById(id);
+            if (teamRepository.findAll().contains(teamId)) {
+                Team team = teamRepository.findById(teamId)
+                        .orElseThrow(() -> new NoResultException("Unable to find team by id: " + teamId))
                 team.getId().remove(team);
             }
             else {
