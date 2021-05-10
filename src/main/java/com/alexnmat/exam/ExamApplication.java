7b21ad71ea4a8ddafd6cc7d0e38622d75669e728
Mathias <math8802@stud.kea.dk>
2021-05-11 00:37:28 +0200

    asd

--- src/main/java/com/alexnmat/exam/ExamApplication.java
+++ src/main/java/com/alexnmat/exam/ExamApplication.java
@@ -10,9 +10,4 @@ public class ExamApplication {
         SpringApplication.run(ExamApplication.class, args);
     }
 
-    //TODO: Fix database relationships, mostly MANY TO MANY. maybe ask pete?
-    // https://stackoverflow.com/questions/39802264/jpa-how-to-persist-many-to-many-relation
-    // https://stackoverflow.com/questions/19280121/spring-and-or-hibernate-saving-many-to-many-relations-from-one-side-after-form
-    // https://thorben-janssen.com/hibernate-tips-map-bidirectional-many-many-association/
-    //
 }
